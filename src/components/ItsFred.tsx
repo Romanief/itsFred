df1f4187d01f92107361089362a7213db7060449
Romanief <federicoromaniello@outlook.com>
2023-04-15 18:29:25 +0100

    Fixed image control

--- src/components/ItsFred.tsx
+++ src/components/ItsFred.tsx
@@ -1,13 +1,13 @@
 export default function ItsFred() {
   return (
     <>
-      <div className="relative flex flex-col md:flex-row justify-center mx-auto my-auto sm:top-10 lg:top-1/4">
-        <div className="h-[20vw] w-[20vw] mr-20 mt-10">
-          <div className="absolute hidden md:block bg-potrait h-[20vw] w-[20vw] min-h-40 rounded-full bg-contain transition"></div>
-          <div className="absolute hidden md:block bg-ballet h-[20vw] w-[20vw]  min-w-40 min-h-40 rounded-full bg-contain transition z-50 opacity-0 hover:opacity-100"></div>
+      <div className="relative flex flex-col lg:flex-row justify-center mx-auto my-auto top-10 lg:top-1/4 lg:right-80">
+        <div className="md:h-[20vw] md:w-[20vw] h-40 w-40  lg:mr-20 lg:mt-10 mx-auto">
+          <div className="absolute bg-potrait md:h-[20vw] md:w-[20vw] h-40 w-40 rounded-full bg-contain transition"></div>
+          <div className="absolute bg-ballet md:h-[20vw] md:w-[20vw] h-40 w-40 rounded-full bg-contain transition z-50 opacity-0 hover:opacity-100"></div>
         </div>
 
-        <div className="md:left-1/2 md:top-1/4 md:w-1/3 flex flex-col md:text-left text-center">
+        <div className="lg:top-1/4 lg:w-1/3 flex flex-col lg:text-left text-center">
           <div className="h-1/5"></div>
           <div className="text-9xl">
             <p>Hey,</p>
