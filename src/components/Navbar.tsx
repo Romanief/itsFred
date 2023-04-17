a7a2e7cf9fd7165bd6695fbe4d78899884a43b78
Romanief <federicoromaniello@outlook.com>
2023-04-17 08:46:24 +0100

    Substitute navbar hover with onclick

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { MdMenuBook } from "react-icons/md"
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
 
 function Navbar({
   menuArray,
@@ -9,34 +9,52 @@ function Navbar({
   reference: React.RefObject<HTMLElement>
 }) {
   return (
-    <div className="transition fixed h-screen w-72 -translate-x-60 hover:translate-x-0 hover:text-transparent z-20 sm:shadow-2xl shadow-none sm:shadow-black">
-      <div className="absolute w-full h-full z-2 bg-black/70 -translate-x-16 sm:-translate-x-0"></div>
-      <div className="w-full flex justify-end p-3 sm:ml-10 z-3">
-        <p className="sm:text-5xl text-3xl z-10">
-          <MdMenuBook />
-        </p>
+    <>
+      <div
+        id="navbar"
+        className="transition fixed h-screen w-72 -translate-x-72 z-50 bg-black"
+      >
+        <div className="flex flex-col h-full justify-evenly -translate-y-20 text-3xl pl-9">
+          <AiOutlineClose
+            className=" md:translate-y-5 translate-x-40 cursor-pointer"
+            onClick={() => {
+              document.querySelector("#navbar")!.className =
+                "transition fixed h-screen w-72 -translate-x-72 z-50 bg-black"
+            }}
+          />
+          {menuArray.map((x, i) => {
+            return (
+              <div
+                key={i}
+                className="text-white hover:text-yellow-500 hover:font-bold cursor-pointer"
+                onClick={() => {
+                  reference.current?.scrollTo({
+                    top: window.innerHeight * i,
+                    behavior: "smooth",
+                  })
+                }}
+              >
+                {x}
+              </div>
+            )
+          })}
+        </div>
       </div>
-
-      <div className="flex flex-col h-full justify-evenly -translate-y-20 text-3xl pl-9 z-3">
-        {menuArray.map((x, i) => {
-          return (
-            <div
-              key={i}
-              className="text-white hover:text-yellow-500 hover:font-bold"
-              onClick={() => {
-                reference.current?.scrollTo({
-                  top: window.innerHeight * i,
-                  behavior: "smooth",
-                })
-              }}
-            >
-              {x}
-            </div>
-          )
-        })}
-      </div>
-    </div>
+      <AiOutlineMenu
+        className="text-3xl fixed md:top-10 top-5 left-5 hover:cursor-pointer z-40"
+        onClick={() => {
+          console.log("click")
+          document.querySelector("#navbar")!.className =
+            "transition fixed h-screen w-72 translate-x-0 z-50 bg-black"
+        }}
+      />
+    </>
   )
 }
 
 export default Navbar
+
+/* 
+      
+
+      */
