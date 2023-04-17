9da978de9118b0ec27e18a20c1df541d099778a3
Romanief <federicoromaniello@outlook.com>
2023-04-17 14:26:41 +0100

    Addedd github links in projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Carousel from "./Carousel"
 
+import { AiFillGithub } from "react-icons/ai"
+
 function Projects() {
   const list = ["Projects"]
   const shopImages = ["/SA0.jpeg", "/SA1.jpeg", "/SA2.jpeg", "/SA3.jpeg"]
@@ -21,6 +23,15 @@ function Projects() {
             <div className="hidden lg:block">
               Technology used: React, Django, Django-rest, SimpleJWT, Tailwind
             </div>
+            <div className="flex mt-2">
+              <AiFillGithub className="mt-1 mr-2" />
+              <a
+                href="https://github.com/Romanief/Todo"
+                className="hover:text-yellow-600"
+              >
+                Romanief/Todo
+              </a>
+            </div>
           </div>
           <Carousel images={todoImages} />
         </div>
@@ -29,6 +40,15 @@ function Projects() {
             <div className="font-bold">E-commerce App</div>
             <div className="hidden lg:block">
               Technology used: React, Next, Tailwind
+            </div>
+            <div className="flex mt-2">
+              <AiFillGithub className="mt-1 mr-2" />
+              <a
+                className="hover:text-yellow-600"
+                href="https://github.com/Romanief/Shopapp"
+              >
+                Romanief/Shopapp
+              </a>
             </div>
           </div>
           <Carousel images={shopImages} />
