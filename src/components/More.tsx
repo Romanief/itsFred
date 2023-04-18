0e65bf42e8054c4d3ea35d1738a2a09cc9d38f8c
Romanief <federicoromaniello@outlook.com>
2023-04-18 10:30:35 +0100

    Updated cv link

--- src/components/More.tsx
+++ src/components/More.tsx
@@ -7,8 +7,7 @@ function More() {
         <div>Want to know more about me?</div>
         <a
           className=" cursor-auto"
-          href="https://cvws.icloud-content.com/B/AVLXLUoLGITZue37Sud3-agGuV96AdEIHYft5tTYnEF-oboeoYh18rdm/Federico+Romaniello+-+CV+17%3A04%3A2023.pdf?o=AkiBHZI4QgZ27tFCciQDSYzNWdE7uDCaNKrhEfPRyZHX&v=1&x=3&a=CAog4xzX8fr6_GV7_e-TYcY6XhIhA16kI4MBn4Kbv9c5I-sSbxCUqt76-DAYlIe6_PgwIgEAUgQGuV96WgR18rdmaifBQehmP43GBqHlarJbeJYfauQpjeqc44A8s_CddivH6kgE5yw3HdRyJw48e5BArqKm7VeezJRPzZGsCBFx3uKaFMBroeOvENp-pFqWjYn0hg&e=1681740694&fl=&r=76bb9813-d555-409a-93b1-fff4b53563d1-1&k=fn2Dzp3NsRvmMOK6VFSZAw&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=29&s=7SCGMRI_qmEyriQwgpHCGTXL0F4&cd=i
-          "
+          href="https://online.flippingbook.com/view/647299696/"
         >
           See my CV <span className="cursor-pointer text-yellow-600">here</span>
         </a>
